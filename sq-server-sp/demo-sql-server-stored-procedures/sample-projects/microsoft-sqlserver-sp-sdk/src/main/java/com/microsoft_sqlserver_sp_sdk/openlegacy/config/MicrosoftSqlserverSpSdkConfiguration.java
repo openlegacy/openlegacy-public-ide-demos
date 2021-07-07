440ac0529f34e87acdbc99a460f07e1a7d828ec1
Ruslan Abramov <ruslan1abramov@gmail.com>
2021-07-07 08:49:11 +0300

    Delete sq-server-sp directory

--- sq-server-sp/demo-sql-server-stored-procedures/sample-projects/microsoft-sqlserver-sp-sdk/src/main/java/com/microsoft_sqlserver_sp_sdk/openlegacy/config/MicrosoftSqlserverSpSdkConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.microsoft_sqlserver_sp_sdk.openlegacy.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class MicrosoftSqlserverSpSdkConfiguration {
-}
